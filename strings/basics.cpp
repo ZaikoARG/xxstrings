5e69b53004c49ff134fc706c68e74c698ac09815
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-11-21 23:33:24 +0100

    Update basics.cpp

--- strings/basics.cpp
+++ strings/basics.cpp
@@ -3,31 +3,41 @@
 
 void PrintLastError(LPTSTR lpszFunction)
 {
-	// Retrieve the system error message for the last-error code
+    // Retrieve the system error message for the last-error code
     LPVOID lpMsgBuf;
-    LPVOID lpDisplayBuf;
-    DWORD dw = GetLastError(); 
+    LPTSTR lpDisplayBuf = NULL; // Initialize lpDisplayBuf to NULL
+    DWORD dw = GetLastError();
 
     FormatMessage(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
         FORMAT_MESSAGE_FROM_SYSTEM |
         FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL,
         dw,
         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        (LPTSTR) &lpMsgBuf,
-        0, NULL );
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
 
-    // Display the error message and exit the process
-    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
-        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
-    StringCchPrintf((LPTSTR)lpDisplayBuf, 
-        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-        TEXT("%s failed with error %d: %s"), 
-        lpszFunction, dw, lpMsgBuf); 
+    // Allocate memory for the display buffer and check for allocation success
+    lpDisplayBuf = (LPTSTR)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
 
-	fwprintf(stderr,(LPCTSTR) lpDisplayBuf );
+    if (lpDisplayBuf != NULL) {
+        // Format and print the error message
+        StringCchPrintf(lpDisplayBuf,
+            LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+            TEXT("%s failed with error %d: %s"),
+            lpszFunction, dw, (LPCTSTR)lpMsgBuf);  // Corrected the argument type
+
+        fwprintf(stderr, lpDisplayBuf);
+
+        // Free allocated memory
+        LocalFree(lpDisplayBuf);
+    }
+    else {
+        // Handle the case when memory allocation fails
+        fwprintf(stderr, TEXT("Failed to allocate memory for error message."));
+    }
 
     LocalFree(lpMsgBuf);
-    LocalFree(lpDisplayBuf);
 }
