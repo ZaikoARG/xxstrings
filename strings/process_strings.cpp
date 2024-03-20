a797ca0336b5d06236a5ff1a0fd7c4c7605169ab
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-20 08:07:49 +0100

    Update process_strings.cpp

--- strings/process_strings.cpp
+++ strings/process_strings.cpp
@@ -2,7 +2,6 @@
 #include "process_strings.h"
 #include "string_parser.h"
 
-
 static bool IsWin64(HANDLE process)
 {
 	BOOL retVal;
@@ -70,152 +69,144 @@ process_strings::process_strings(string_parser* parser)
 
 bool process_strings::processAllHeaps(HANDLE ph, char* process_name, bool ecomode, bool pagination)
 {
-	// Set the max address of the target process. Assume it is a 64 bit process.
-	__int64 maxAddress = 0;
-	maxAddress = 0x7ffffffffff;
-	// Walk the process heaps
-	__int64 address = 0;
-	bool paging = true;
-	MEMORY_BASIC_INFORMATION mbi;
-	PROCESS_MEMORY_COUNTERS_EX pmi;
-	GetProcessMemoryInfo(ph, (PROCESS_MEMORY_COUNTERS*)&pmi, sizeof(pmi));
+    // Set the max address of the target process. Assume it is a 64 bit process.
+    __int64 maxAddress = 0x7FFFFFFFFFF;
+    // Walk the process heaps
+    __int64 address = 0;
+    bool paging = true;
+    MEMORY_BASIC_INFORMATION mbi;
+    PROCESS_MEMORY_COUNTERS_EX pmi;
+    GetProcessMemoryInfo(ph, (PROCESS_MEMORY_COUNTERS*)&pmi, sizeof(pmi));
 
-	if (ecomode == true) {
-		while (address < maxAddress)
-		{
-			if (pmi.PrivateUsage < 524288000) {
-				paging = false;
-			}
-			// Load this heap information
-			__int64 blockSize = VirtualQueryEx(ph, (LPCVOID)address, (_MEMORY_BASIC_INFORMATION*)&mbi, sizeof(_MEMORY_BASIC_INFORMATION64));
-			__int64 newAddress = (__int64)mbi.BaseAddress + (__int64)mbi.RegionSize + (__int64)1;
-			if (newAddress <= address)
-				break;
-			address = newAddress;
+    if (ecomode)
+    {
+        while (address < maxAddress)
+        {
+            if (pmi.PrivateUsage < 524288000)
+            {
+                paging = false;
+            }
+            // Load this heap information
+            __int64 blockSize = VirtualQueryEx(ph, (LPCVOID)address, reinterpret_cast<MEMORY_BASIC_INFORMATION*>(&mbi), sizeof(MEMORY_BASIC_INFORMATION));
+            __int64 newAddress = (__int64)mbi.BaseAddress + (__int64)mbi.RegionSize + (__int64)1;
+            if (newAddress <= address)
+                break;
+            address = newAddress;
 
-			if (mbi.State == MEM_COMMIT && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && mbi.RegionSize <= 5000000 && paging == true || mbi.State == MEM_COMMIT && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && paging == false)
-			{
-				// Process this heap
+            if ((mbi.State == MEM_COMMIT) && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && ((mbi.RegionSize <= 5000000 && paging) || (!paging)))
+            {
+                // Process this heap
 
-				// Read in the heap
-				unsigned char* buffer = new unsigned char[mbi.RegionSize];
-				if (buffer != NULL)
-				{
-					__int64 numRead = 0;
-					bool result = ReadProcessMemory(ph, (LPCVOID)mbi.BaseAddress, buffer, mbi.RegionSize, (SIZE_T*)&numRead);
+                // Read in the heap
+                std::unique_ptr<unsigned char[]> buffer(new unsigned char[mbi.RegionSize]);
+                if (buffer)
+                {
+                    __int64 numRead = 0;
+                    bool result = ReadProcessMemory(ph, (LPCVOID)mbi.BaseAddress, buffer.get(), mbi.RegionSize, (SIZE_T*)&numRead);
 
-					//fprintf(stderr,"Current address: %016llX\n",mbi.BaseAddress);
-					if (numRead > 0)
-					{
-						if (numRead != static_cast<unsigned __int64>(mbi.RegionSize))
-						{
-							DWORD errorMessageID = GetLastError();
-							LPVOID errorMessageBuffer = nullptr;
+                    if (numRead > 0)
+                    {
+                        if (numRead != static_cast<unsigned __int64>(mbi.RegionSize))
+                        {
+                            DWORD errorMessageID = GetLastError();
+                            LPVOID errorMessageBuffer = nullptr;
 
-							FormatMessage(
-								FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-								nullptr,
-								errorMessageID,
-								0, // Default language
-								reinterpret_cast<LPWSTR>(&errorMessageBuffer),
-								0,
-								nullptr
-							);
+                            FormatMessage(
+                                FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                                nullptr,
+                                errorMessageID,
+                                0, // Default language
+                                reinterpret_cast<LPWSTR>(&errorMessageBuffer),
+                                0,
+                                nullptr
+                            );
 
-							fprintf(stderr, "Failed read full heap from address 0x%016llX: %s. Only %lld of expected %lld bytes were read.\n",
-								reinterpret_cast<uintptr_t>(mbi.BaseAddress), static_cast<const wchar_t*>(errorMessageBuffer),
-								static_cast<long long>(numRead), static_cast<long long>(mbi.RegionSize));
+                            std::cerr << "Failed read full heap from address 0x" << reinterpret_cast<uintptr_t>(mbi.BaseAddress)
+                                << ": " << static_cast<const wchar_t*>(errorMessageBuffer) << ". Only " << numRead
+                                << " of expected " << mbi.RegionSize << " bytes were read." << std::endl;
 
-							LocalFree(errorMessageBuffer);
-						}
+                            LocalFree(errorMessageBuffer);
+                        }
 
-						// Print the strings from this heap
-						parser->parse_block(buffer, numRead, process_name);
-					}
-					else if (!result)
-					{
-						DWORD errorMessageID = GetLastError();
-						LPVOID errorMessageBuffer = nullptr;
+                        // Print the strings from this heap
+                        parser->parse_block(buffer.get(), numRead, process_name);
+                    }
+                    else if (!result)
+                    {
+                        DWORD errorMessageID = GetLastError();
+                        LPVOID errorMessageBuffer = nullptr;
 
-						FormatMessage(
-							FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-							nullptr,
-							errorMessageID,
-							0, // Default language
-							reinterpret_cast<LPWSTR>(&errorMessageBuffer),
-							0,
-							nullptr
-						);
+                        FormatMessage(
+                            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                            nullptr,
+                            errorMessageID,
+                            0, // Default language
+                            reinterpret_cast<LPWSTR>(&errorMessageBuffer),
+                            0,
+                            nullptr
+                        );
 
-						fprintf(stderr, "Failed to read from address 0x%p. %s", mbi.BaseAddress, static_cast<const wchar_t*>(errorMessageBuffer));
+                        std::cerr << "Failed to read from address 0x" << mbi.BaseAddress << ". " << static_cast<const wchar_t*>(errorMessageBuffer) << std::endl;
 
-						LocalFree(errorMessageBuffer);
-					}
+                        LocalFree(errorMessageBuffer);
+                    }
+                }
+                else {
+                    std::cerr << "Failed to allocate space of " << mbi.RegionSize << " for reading in a heap." << std::endl;
+                }
+            }
+        }
 
-					// Cleanup
-					delete[] buffer;
-				}
-				else {
-					fprintf(stderr, "Failed to allocate space of %lld for reading in a heap.", static_cast<long long>(mbi.RegionSize));
-				}
-			}
-		}
+        return true;
+    }
+    else
+    {
+        if (!pagination) {
+            paging = false;
+        }
+        while (address < maxAddress)
+        {
+            __int64 blockSize = VirtualQueryEx(ph, (LPCVOID)address, reinterpret_cast<MEMORY_BASIC_INFORMATION*>(&mbi), sizeof(MEMORY_BASIC_INFORMATION));
+            __int64 newAddress = (__int64)mbi.BaseAddress + (__int64)mbi.RegionSize + (__int64)1;
+            if (newAddress <= address)
+                break;
+            address = newAddress;
 
-		return true;
-	}
-	else
-	{
-		if (pagination == false) {
-			paging = false;
-		}
-		while (address < maxAddress)
-		{
-			// Load this heap information
-			__int64 blockSize = VirtualQueryEx(ph, (LPCVOID)address, (_MEMORY_BASIC_INFORMATION*)&mbi, sizeof(_MEMORY_BASIC_INFORMATION64));
-			__int64 newAddress = (__int64)mbi.BaseAddress + (__int64)mbi.RegionSize + (__int64)1;
-			if (newAddress <= address)
-				break;
-			address = newAddress;
+            if ((mbi.State == MEM_COMMIT) && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && ((mbi.RegionSize <= 5000000 && paging) || (!paging)))
+            {
+                std::unique_ptr<unsigned char[]> buffer(new unsigned char[mbi.RegionSize]);
+                if (buffer)
+                {
+                    __int64 numRead = 0;
+                    bool result = ReadProcessMemory(ph, (LPCVOID)mbi.BaseAddress, buffer.get(), mbi.RegionSize, (SIZE_T*)&numRead);
 
-			if (mbi.State == MEM_COMMIT && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && mbi.RegionSize <= 5000000 && paging == true || mbi.State == MEM_COMMIT && !(mbi.Protect & (PAGE_NOACCESS | PAGE_GUARD)) && paging == false)
-			{
-				// Process this heap
+                    if (numRead > 0)
+                    {
+                        if (numRead != static_cast<unsigned __int64>(mbi.RegionSize))
+                        {
+                            char errorMsg[256]{};
+                            std::cerr << "Failed read full heap from address 0x" << mbi.BaseAddress
+                                << ": " << errorMsg << ". Only " << numRead << " of expected "
+                                << mbi.RegionSize << " bytes were read." << std::endl;
+                        }
 
-				// Read in the heap
-				unsigned char* buffer = new unsigned char[mbi.RegionSize];
-				if (buffer != NULL)
-				{
-					__int64 numRead = 0;
-					bool result = ReadProcessMemory(ph, (LPCVOID)mbi.BaseAddress, buffer, mbi.RegionSize, (SIZE_T*)&numRead);
+                        parser->parse_block(buffer.get(), numRead, process_name);
+                    }
+                    else if (!result)
+                    {
+                        char errorMsg[256]{};
+                        std::cerr << "Failed to read from address 0x" << mbi.BaseAddress << ". " << errorMsg << std::endl;
+                        PrintLastError(L"ReadProcessMemory");
+                    }
+                }
+                else {
+                    std::cerr << "Failed to allocate space of " << mbi.RegionSize << " for reading in a heap." << std::endl;
+                }
+            }
+        }
 
-					char errorMsg[256]{}; // Adjust the size as needed
-
-					//fprintf(stderr,"Current address: %016llX\n",mbi.BaseAddress);
-					if (numRead > 0)
-					{
-						if (numRead != static_cast<unsigned __int64>(mbi.RegionSize))
-							fprintf(stderr, "Failed read full heap from address 0x%016llX: %s. Only %lld of expected %lld bytes were read.\n",
-								mbi.BaseAddress, errorMsg, static_cast<long long>(numRead), static_cast<long long>(mbi.RegionSize));
-
-						// Print the strings from this heap
-						parser->parse_block(buffer, numRead, process_name);
-					}
-					else if (!result) {
-						fprintf(stderr, "Failed to read from address 0x%016llX. %s", mbi.BaseAddress, errorMsg);
-						PrintLastError(L"ReadProcessMemory");
-					}
-
-					// Cleanup
-					delete [] buffer;
-				}
-				else {
-					fprintf(stderr, "Failed to allocate space of %lld for reading in a heap.", static_cast<long long>(mbi.RegionSize));
-				}
-			}
-		}
-
-		return true;
-	}
+        return true;
+    }
 }
 
 void process_strings::generateModuleList(HANDLE hSnapshot)
@@ -237,6 +228,4 @@ void process_strings::generateModuleList(HANDLE hSnapshot)
 	}
 }
 
-process_strings::~process_strings(void)
-{
-}
+process_strings::~process_strings(void) {}
